38557f7bfa4856680fab7057e47fcebcf0e2bceb
341 on 105 <838839085@qq.com>
2026-04-23 10:34:47 +0800

    feat: Update training configuration and refactor data handling in mergeall.yaml and infer.py

--- solarchip/main/infer.py
+++ solarchip/main/infer.py
@@ -41,12 +41,6 @@ def parse_args():
 
     return parser.parse_args()
 
-if torch.cuda.is_available():
-    gpu_name = torch.cuda.get_device_name(0)
-    if "A40" in gpu_name or "A100" in gpu_name:
-        torch.set_float32_matmul_precision('high') # highest, high, medium
-        print(f'device is {gpu_name}, set float32_matmul_precision to high')
-
 
 model = None
 
